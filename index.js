2a28e5260296d951161ac6d1a872bcb122b7c272
Shaun <shaun@thenetninja.co.uk>
2020-09-10 17:56:47 +0100

    lesson-13

--- index.js
+++ index.js
@@ -26,3 +26,16 @@ tabs.forEach(tab => {
     })
   })
 })
+
+// modal
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+  modal.classList.add('is-active');
+})
+
+modalBg.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+})
