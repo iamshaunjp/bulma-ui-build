ca803e9941c5b465c26b8a827147229e52258c19
Shaun <shaun@thenetninja.co.uk>
2020-09-10 17:11:08 +0100

    lesson-5

---
+++ index.js
@@ -0,0 +1,7 @@
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
